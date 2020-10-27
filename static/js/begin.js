e5bbba82c201530d1398e82b9b8c573c4dbbf8e1
BozeBro <ben.ozua@gmail.com>
2020-10-26 21:23:46 -0500

    minor changes

---
+++ static/js/begin.js
@@ -0,0 +1,10 @@
+(function(){
+    "use strict";
+    /* Start of your code */
+    function greetMe(yourName) {
+      console.log(1 + 1);
+    }
+    
+    greetMe('World');
+    /* End of your code */
+  })();
