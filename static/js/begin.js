d4a947fc1cc5c8ccbede4267c2afbe230e79750d
BozeBro <ben.ozua@gmail.com>
2020-11-10 11:59:03 -0600

    Created a board. Not a canvas though

--- static/js/begin.js
+++ static/js/begin.js
@@ -1,10 +1,21 @@
-(function(){
-    "use strict";
-    /* Start of your code */
-    function greetMe(yourName) {
-      console.log(1 + 1);
+var chainReaction = function() {
+  var board = new Array()
+  for (h=0;h<height;h++) {
+    board.push(new Array(width))
+    for (w=0;w<width;w++) {
+      board[h][w] = w
     }
-    
-    greetMe('World');
-    /* End of your code */
-  })();
+  }
+  return board
+}
+function move(board, x, y, p) {
+  if (board[y][x][1] === p || board[y][x][1] === null) {
+    board[y][x][0] += 1
+  }
+}
+(function() {
+  var board = initBoard(5, 5);
+  console.log(board)
+
+}
+)();
