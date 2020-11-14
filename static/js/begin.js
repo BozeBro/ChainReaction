615ca9ad7475302334a91a915733b395e4e1e2e9
BozeBro <ben.ozua@gmail.com>
2020-11-14 14:16:01 -0600

    solidy canvas stuff

--- static/js/begin.js
+++ static/js/begin.js
@@ -1,21 +1,39 @@
-var chainReaction = function() {
-  var board = new Array()
-  for (h=0;h<height;h++) {
-    board.push(new Array(width))
-    for (w=0;w<width;w++) {
-      board[h][w] = w
+const canvas = document.getElementById("chainReaction");
+const ctx = canvas.getContext("2d");
+var squares = [];
+var squareLength = null
+
+function initBoard(x, y) {
+  height = 450;
+  width = 450;
+
+  squareLength = Math.min(width / x, height / y);
+
+  ctx.canvas.width = x * squareLength;
+  ctx.canvas.height = y * squareLength;
+  ctx.fillStyle = "#fff";
+
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (h = 0; h < y; h++) {
+    squares[h] = [];
+    for (l = 0; l < x; l++) {
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.strokeRect(l * squareLength, h * squareLength, squareLength, squareLength);
+      ctx.closePath();
+      squares[h][l] = 0;
     }
   }
-  return board
+  return squareLength
+};
+function makeCircle() {
+  ctx.beginPath();
+  ctx.fillStyle = "#f00";
+  ctx.lineWidth = 1;
+  ctx.fillRect(1*squareLength + squareLength / 3, 1 * squareLength + squareLength / 3, squareLength/3, squareLength/3);
+  //ctx.fillRect()
+  ctx.closePath();
 }
-function move(board, x, y, p) {
-  if (board[y][x][1] === p || board[y][x][1] === null) {
-    board[y][x][0] += 1
-  }
-}
-(function() {
-  var board = initBoard(5, 5);
-  console.log(board)
-
-}
-)();
+squareLength = initBoard(20, 20);
+makeCircle();
