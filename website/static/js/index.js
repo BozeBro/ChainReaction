09cd4569e4e1e6b4a1db3cd6e19f66f26ab79232
BozeBro <ben.ozua@gmail.com>
2020-12-21 23:57:38 -0600

    neglible change. testing

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -22,10 +22,12 @@ let joinHandler = () => {
 
 }
 let btnClicked = (e) => {
+    console.log("YESSIR")
     const btnClicked = e.target.nodeName === "BUTTON";
     if (!btnClicked) { return }
     switch (e.target.className) {
         case "ext":
+            console.log("exit")
             popup.style.display = "none";
             popup = null;
             errCre.innerHTML = "";
@@ -33,6 +35,7 @@ let btnClicked = (e) => {
             break;
         case "ent":
             if (popup.id === "pop-join") {
+                console.log("join")
                 let room = document.getElementById("room").value;
                 let pin = document.getElementById("pin").value;
                 if (!(room && pin)) { return }
@@ -46,6 +49,7 @@ let btnClicked = (e) => {
                     })
 
             } else {
+                console.log("sent")
                 // Creating a room
                 let DOMName = document.getElementById("name");
                 let room = DOMName.value;
@@ -57,7 +61,6 @@ let btnClicked = (e) => {
                 DOMName.value = ""
                 fetch("http://" + document.location.host + "/api/create", {
                     method: 'POST',
-                    redirect: 'follow',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ Players: players, Room: room }),
                 })
@@ -68,16 +71,16 @@ let btnClicked = (e) => {
                             case 400:
                                 errCre.innerHTML = "WHAT ARE YOU DOING?";
                         }
-                        return res
-                    }).then(res => {
+                        //return res
+                    })
+                    /*.then(res => {
                         if (!res.redirected) return;
                         window.location.replace(res.url);
 
-                    })
+                    }) */
             }
     }
 }
-var m;
 window.onload = () => {
     spaces.addEventListener("click", btnClicked)
     cre.addEventListener("click", creHandler)
