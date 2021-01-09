30ad9166961fc943fe98f9e28b6e72b00baf9330
BozeBro <ben.ozua@gmail.com>
2021-01-09 11:34:25 -0600

    Added intervaled updates to max players that played the game.

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/BozeBro/ChainReaction/server"
 )
 
+// SUID | RW | RW | RW
+const SUIDRWRR = 4664
+
 func main() {
 	// Makes it so that rand functions are actually pseudo random
 	// Addr := 76.192.124.46
 	rand.Seed(time.Now().UnixNano())
-	r := server.MakeRouter()
+	finish := make(chan error)
+	r, playerCounter := server.MakeRouter()
+	ticker := time.NewTicker(time.Second * 24)
 	srv := &http.Server{
 		Handler: r,
 		Addr:    "127.0.0.1:8080",
@@ -21,6 +29,42 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	log.Println("127.0.0.1:8080")
-	log.Fatal(srv.ListenAndServe())
+	log.Println("Starting on 127.0.0.1:8080")
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				var oldVal int
+				file, err := ioutil.ReadFile("data.txt")
+				if err != nil {
+					err := ioutil.WriteFile("data.txt", []byte(fmt.Sprintf("%d", playerCounter.TotalPlayers)), SUIDRWRR)
+					if err != nil {
+						log.Println("Main.go:line 40: The file probably didn't exist.", err)
+					}
+					continue
+				}
+				if val := string(file); val == "" || val == " " {
+					oldVal = 0
+				} else {
+					oldVal, err = strconv.Atoi(val)
+					if err != nil {
+						log.Printf("The value in the file was %d and the error was %s", oldVal, err)
+						oldVal = 0
+					}
+				}
+				if playerCounter.TotalPlayers > oldVal {
+					err := ioutil.WriteFile("data.txt", []byte(fmt.Sprintf("%d", playerCounter.TotalPlayers)), SUIDRWRR)
+					if err != nil {
+						log.Printf("The error is occuring in the main file. Trying to write to a file. %s", err)
+					}
+				}
+			case err := <-finish:
+				log.Fatal("The server is dying down", err)
+				return
+			}
+		}
+	}()
+	err := srv.ListenAndServe()
+	finish <- err
+
 }
