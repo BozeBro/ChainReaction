881e5b8adb9477fa7b99f46fd681a50e3530890c
BozeBro <ben.ozua@gmail.com>
2021-01-09 12:06:41 -0600

    lets try local server first

--- main.go
+++ main.go
@@ -24,12 +24,12 @@ func main() {
 	ticker := time.NewTicker(time.Second * 24)
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "chainreactionsite.herokuapp.com:8000",
+		Addr:    "127.0.0.1:8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	log.Println("Starting on chainreactionsite.herokuapp.com:8000")
+	log.Println("Starting on 127.0.0.1:8000")
 	go func() {
 		for {
 			select {
