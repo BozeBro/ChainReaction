1b765773bc3e62084c39219379a1b1721990628b
BozeBro <ben.ozua@gmail.com>
2021-01-09 11:53:06 -0600

    make homescreen much more viewable

--- main.go
+++ main.go
@@ -24,12 +24,12 @@ func main() {
 	ticker := time.NewTicker(time.Second * 24)
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	log.Println("Starting on 127.0.0.1:8080")
+	log.Println("Starting on 127.0.0.1:8000")
 	go func() {
 		for {
 			select {
