6a0795393060d1f70a40cee149477301fce8b145
BozeBro <ben.ozua@gmail.com>
2021-02-11 17:00:05 -0600

    lesss waytime = more fun

--- server/handshake.go
+++ server/handshake.go
@@ -112,7 +112,7 @@ func WSHandshake(w http.ResponseWriter, r *http.Request, roomStorage Storage) {
 									_, sq := hub.Match.Max(
 										botclient.Color,
 										nextColor,
-										3,
+										1,
 										a,
 										b,
 										-6,
