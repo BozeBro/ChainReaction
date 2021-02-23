539d4c3424310011e931ecfba8d8f2c10f45d4f6
BozeBro <ben.ozua@gmail.com>
2021-02-22 20:11:19 -0600

    added timestamps

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,7 +1,11 @@
 
 function MessageAdd(message, username, color) {
 	var chat_messages = document.getElementById("chat-messages");
-    msg = `<div><span style="color:${escapeHtml(color)}">${escapeHtml(username)}</span> ${escapeHtml(message)}</div>`
+	var date = new Date();
+	var data = changeHours(Number(date.getHours()));
+	var hours = data[0];
+	var meridian = data[1];
+    msg = `<div><span style="color:purple">${"| "+hours+":"+date.getMinutes()+" "+meridian+" | "}</span><span style="color:${escapeHtml(color)}">${escapeHtml(username)}</span> ${escapeHtml(message)}</div>`
 	chat_messages.insertAdjacentHTML("beforeend", msg);
 	chat_messages.scrollTop = chat_messages.scrollHeight;
 }
@@ -18,6 +22,12 @@ function escapeHtml(text) {
 	return text.replace(/[&<>"']/g, (m) => map[m]);
   }
 
+  function changeHours(hours) {
+	  if (hours > 12) {
+		  return [(hours-12).toString(), "PM"]
+		}
+		return [hours.toString(), "AM"]
+  }
 var beginning;
 var curTime;
 function timing(timestamp) {
